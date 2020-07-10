6534251efd47a6471d0c8714ca064f3d8b343013
Patrick Mackridge <pat.mack@talktalk.net>
2020-07-10 16:13:39 +0100

    QuizLogic class generates a valid, tested equation string

--- app/src/main/java/com/example/apprithmetic/QuizLogic.java
+++ app/src/main/java/com/example/apprithmetic/QuizLogic.java
@@ -5,7 +5,11 @@ public class QuizLogic {
     int solution;
 
     public void generateEquation() {
-
+        long num1 = Math.round(Math.random() * 100);
+        long num2 = Math.round(Math.random() * 100);
+        String[] operators = {"+", "-"};
+        String operator = operators[ (int) Math.round(Math.random())];
+        equation = String.format("%s %s %s", num1, operator, num2);
     }
 
     public String getEquation() {
